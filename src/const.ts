8da900155590cb747a9924f2f9ff902d80dc21a9
ohmypsc <greatkto@gmail.com>
2025-10-28 15:15:44 +0900

    Update const.ts

--- src/const.ts
+++ src/const.ts
@@ -10,51 +10,29 @@ dayjs.locale("ko")
 export { dayjs }
 
 export const WEDDING_DATE = dayjs.tz("2025-07-11 11:00", "Asia/Seoul")
-export const WEDDING_DATE_FORMAT = `YYYY년 MMMM D일 dddd A h시${WEDDING_DATE.minute() === 0 ? "" : " m분"}`
+export const WEDDING_DATE_FORMAT = `YYYY년 MMMM D일 dddd A h시${
+  WEDDING_DATE.minute() === 0 ? "" : " m분"
+}`
 
-// 예식 당월 휴무일. 켈린더에 표시하기 위함.
-// 예: 예식일 8월 -> 8월 15일 광복절
-export const HOLIDAYS = [15]
 
-export const LOCATION = "유성컨벤션웨딩홀import dayjs from "dayjs"
-import utc from "dayjs/plugin/utc"
-import timezone from "dayjs/plugin/timezone"
-import "dayjs/locale/ko"
-
-dayjs.extend(utc)
-dayjs.extend(timezone)
-dayjs.locale("ko")
-
-export { dayjs }
-
-export const WEDDING_DATE = dayjs.tz("2025-07-11 11:00", "Asia/Seoul")
-export const WEDDING_DATE_FORMAT = `YYYY년 MMMM D일 dddd A h시${WEDDING_DATE.minute() === 0 ? "" : " m분"}`
-
-// 예식 당월 휴무일. 켈린더에 표시하기 위함.
-// 예: 예식일 8월 -> 8월 15일 광복절
-export const HOLIDAYS = [15]
-
-export const LOCATION = "유성컨벤션웨딩홀"
+// 📍 장소 정보
+export const LOCATION = "유성컨벤션웨딩홀 3층 그랜드홀"
 export const LOCATION_ADDRESS = "대전 유성구 온천북로 77"
 
-// 카카오톡 공유 시 위치 정보로 사용할 주소.
-// LOCATION 과 동일하게 설정해도 무방하나, 필요에 따라 좀 더 상세히 작성 가능.
+// 카카오톡 공유 시 표시될 주소
 export const SHARE_ADDRESS = LOCATION
 export const SHARE_ADDRESS_TITLE = LOCATION
 
-// 네이버 지도 및 카카오 네비게이션에 사용할 좌표. [경도, 위도] 형식.
+// 지도 좌표 (유성컨벤션웨딩홀 기준)
 export const WEDDING_HALL_POSITION = [127.3514617, 36.3562313]
 
-// 네이버 지도의 웨딩홀 장소 ID
-// 네이버 지도 웹페이지에서 웨딩홀 검색 후 URL에서 확인 가능.
-// 예: https://map.naver.com/p/entry/place/13321741 -> 13321741
+// 네이버 지도 place ID
 export const NMAP_PLACE_ID = 19882195
 
-// 카카오 지도의 웨딩홀 장소 ID
-// 카카오 지도 웹페이지에서 웨딩홀 검색 후 해당 장소에서 상세보기 클릭 시 URL에서 확인 가능.
-// 예: https://place.map.kakao.com/8634826 -> 8634826
+// 카카오 지도 place ID
 export const KMAP_PLACE_ID = 10457235
 
+// 👰 신부 정보
 export const BRIDE_FULLNAME = "오미영"
 export const BRIDE_FIRSTNAME = "미영"
 export const BRIDE_TITLE = "딸"
@@ -81,6 +59,7 @@ export const BRIDE_INFO = [
   },
 ]
 
+// 🤵 신랑 정보
 export const GROOM_FULLNAME = "백승철"
 export const GROOM_FIRSTNAME = "승철"
 export const GROOM_TITLE = "아들"
