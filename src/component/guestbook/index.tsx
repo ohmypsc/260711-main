c083cd9f0908727a8d426429898ae28c31a66c5a
ohmypsc <greatkto@gmail.com>
2025-10-29 10:09:14 +0900

    Update index.tsx

--- src/component/guestbook/index.tsx
+++ src/component/guestbook/index.tsx
@@ -3,7 +3,6 @@ import { Button } from "../button"
 import { dayjs } from "../../const"
 import { LazyDiv } from "../lazyDiv"
 import { useModal } from "../modal"
-import offlineGuestBook from "./offlineGuestBook.json"
 import { SERVER_URL } from "../../env"
 
 const RULES = {
@@ -31,25 +30,17 @@ type Post = {
 
 export const GuestBook = () => {
   const { openModal, closeModal } = useModal()
-
   const [posts, setPosts] = useState<Post[]>([])
 
   const loadPosts = async () => {
-    if (SERVER_URL) {
-      try {
-        const res = await fetch(
-          `${SERVER_URL}/guestbook?offset=${0}&limit=${3}`,
-        )
-        if (res.ok) {
-          const data = await res.json()
-
-          setPosts(data.posts)
-        }
-      } catch (error) {
-        console.error("Error loading posts:", error)
+    try {
+      const res = await fetch(`${SERVER_URL}/guestbook?offset=0&limit=3`)
+      if (res.ok) {
+        const data = await res.json()
+        setPosts(data.posts)
       }
-    } else {
-      setPosts(offlineGuestBook.slice(0, 3))
+    } catch (error) {
+      console.error("Error loading posts:", error)
     }
   }
 
@@ -69,39 +60,37 @@ export const GuestBook = () => {
             <button
               className="close-button"
               onClick={async () => {
-                if (SERVER_URL) {
-                  openModal({
-                    className: "delete-guestbook-modal",
-                    closeOnClickBackground: false,
-                    header: <div className="title">삭제하시겠습니까?</div>,
-                    content: (
-                      <DeleteGuestBookModal
-                        postId={post.id}
-                        onSuccess={() => {
-                          loadPosts()
-                        }}
-                      />
-                    ),
-                    footer: (
-                      <>
-                        <Button
-                          buttonStyle="style2"
-                          type="submit"
-                          form="guestbook-delete-form"
-                        >
-                          삭제하기
-                        </Button>
-                        <Button
-                          buttonStyle="style2"
-                          className="bg-light-grey-color text-dark-color"
-                          onClick={closeModal}
-                        >
-                          닫기
-                        </Button>
-                      </>
-                    ),
-                  })
-                }
+                openModal({
+                  className: "delete-guestbook-modal",
+                  closeOnClickBackground: false,
+                  header: <div className="title">삭제하시겠습니까?</div>,
+                  content: (
+                    <DeleteGuestBookModal
+                      postId={post.id}
+                      onSuccess={() => {
+                        loadPosts()
+                      }}
+                    />
+                  ),
+                  footer: (
+                    <>
+                      <Button
+                        buttonStyle="style2"
+                        type="submit"
+                        form="guestbook-delete-form"
+                      >
+                        삭제하기
+                      </Button>
+                      <Button
+                        buttonStyle="style2"
+                        className="bg-light-grey-color text-dark-color"
+                        onClick={closeModal}
+                      >
+                        닫기
+                      </Button>
+                    </>
+                  ),
+                })
               }}
             />
           </div>
@@ -119,48 +108,45 @@ export const GuestBook = () => {
 
       <div className="break" />
 
-      {SERVER_URL && (
-        <>
-          <Button
-            onClick={() =>
-              openModal({
-                className: "write-guestbook-modal",
-                closeOnClickBackground: false,
-                header: (
-                  <div className="title-group">
-                    <div className="title">방명록 작성하기</div>
-                    <div className="subtitle">
-                      신랑, 신부에게 축하의 마음을 전해주세요.
-                    </div>
-                  </div>
-                ),
-                content: <WriteGuestBookModal loadPosts={loadPosts} />,
-                footer: (
-                  <>
-                    <Button
-                      buttonStyle="style2"
-                      type="submit"
-                      form="guestbook-write-form"
-                    >
-                      저장하기
-                    </Button>
-                    <Button
-                      buttonStyle="style2"
-                      className="bg-light-grey-color text-dark-color"
-                      onClick={closeModal}
-                    >
-                      닫기
-                    </Button>
-                  </>
-                ),
-              })
-            }
-          >
-            방명록 작성하기
-          </Button>
-          <div className="break" />
-        </>
-      )}
+      <Button
+        onClick={() =>
+          openModal({
+            className: "write-guestbook-modal",
+            closeOnClickBackground: false,
+            header: (
+              <div className="title-group">
+                <div className="title">방명록 작성하기</div>
+                <div className="subtitle">
+                  신랑, 신부에게 축하의 마음을 전해주세요.
+                </div>
+              </div>
+            ),
+            content: <WriteGuestBookModal loadPosts={loadPosts} />,
+            footer: (
+              <>
+                <Button
+                  buttonStyle="style2"
+                  type="submit"
+                  form="guestbook-write-form"
+                >
+                  저장하기
+                </Button>
+                <Button
+                  buttonStyle="style2"
+                  className="bg-light-grey-color text-dark-color"
+                  onClick={closeModal}
+                >
+                  닫기
+                </Button>
+              </>
+            ),
+          })
+        }
+      >
+        방명록 작성하기
+      </Button>
+
+      <div className="break" />
 
       <Button
         onClick={() =>
@@ -231,9 +217,7 @@ const WriteGuestBookModal = ({ loadPosts }: { loadPosts: () => void }) => {
             return
           }
           if (password.length > RULES.password.maxLength) {
-            alert(
-              `비밀번호를 ${RULES.password.maxLength}자 이하로 입력해주세요.`,
-            )
+            alert(`비밀번호를 ${RULES.password.maxLength}자 이하로 입력해주세요.`)
             return
           }
 
@@ -306,34 +290,21 @@ const AllGuestBookModal = ({
 
   const loadPage = async (page: number) => {
     setCurrentPage(page)
-    if (SERVER_URL) {
-      try {
-        const offset = page * POSTS_PER_PAGE
-        const res = await fetch(
-          `${SERVER_URL}/guestbook?offset=${offset}&limit=${POSTS_PER_PAGE}`,
-        )
-        if (res.ok) {
-          const data = await res.json()
-
-          setPosts(data.posts)
-          setTotalPages(Math.ceil(data.total / POSTS_PER_PAGE))
-          if (data.total < offset) {
-            setCurrentPage(Math.ceil(data.total / POSTS_PER_PAGE) - 1)
-          }
-        }
-      } catch (error) {
-        console.error("Error loading posts:", error)
-      }
-    } else {
-      setCurrentPage(page)
-
-      setPosts(
-        offlineGuestBook.slice(
-          page * POSTS_PER_PAGE,
-          (page + 1) * POSTS_PER_PAGE,
-        ),
+    try {
+      const offset = page * POSTS_PER_PAGE
+      const res = await fetch(
+        `${SERVER_URL}/guestbook?offset=${offset}&limit=${POSTS_PER_PAGE}`,
       )
-      setTotalPages(Math.ceil(offlineGuestBook.length / POSTS_PER_PAGE))
+      if (res.ok) {
+        const data = await res.json()
+        setPosts(data.posts)
+        setTotalPages(Math.ceil(data.total / POSTS_PER_PAGE))
+        if (data.total < offset) {
+          setCurrentPage(Math.ceil(data.total / POSTS_PER_PAGE) - 1)
+        }
+      }
+    } catch (error) {
+      console.error("Error loading posts:", error)
     }
   }
 
@@ -344,7 +315,6 @@ const AllGuestBookModal = ({
   const pages = useMemo(() => {
     const start = Math.floor(currentPage / PAGES_PER_BLOCK) * PAGES_PER_BLOCK
     const end = Math.min(start + PAGES_PER_BLOCK, totalPages)
-
     return Array.from({ length: end - start }).map((_, index) => index + start)
   }, [currentPage, totalPages])
 
@@ -355,41 +325,39 @@ const AllGuestBookModal = ({
           <div className="heading">
             <div
               className="close-button"
-              onClick={async () => {
-                if (SERVER_URL) {
-                  openModal({
-                    className: "delete-guestbook-modal",
-                    closeOnClickBackground: false,
-                    header: <div className="title">삭제하시겠습니까?</div>,
-                    content: (
-                      <DeleteGuestBookModal
-                        postId={post.id}
-                        onSuccess={() => {
-                          loadPosts()
-                          loadPage(currentPage)
-                        }}
-                      />
-                    ),
-                    footer: (
-                      <>
-                        <Button
-                          buttonStyle="style2"
-                          type="submit"
-                          form="guestbook-delete-form"
-                        >
-                          삭제하기
-                        </Button>
-                        <Button
-                          buttonStyle="style2"
-                          className="bg-light-grey-color text-dark-color"
-                          onClick={closeModal}
-                        >
-                          닫기
-                        </Button>
-                      </>
-                    ),
-                  })
-                }
+              onClick={() => {
+                openModal({
+                  className: "delete-guestbook-modal",
+                  closeOnClickBackground: false,
+                  header: <div className="title">삭제하시겠습니까?</div>,
+                  content: (
+                    <DeleteGuestBookModal
+                      postId={post.id}
+                      onSuccess={() => {
+                        loadPosts()
+                        loadPage(currentPage)
+                      }}
+                    />
+                  ),
+                  footer: (
+                    <>
+                      <Button
+                        buttonStyle="style2"
+                        type="submit"
+                        form="guestbook-delete-form"
+                      >
+                        삭제하기
+                      </Button>
+                      <Button
+                        buttonStyle="style2"
+                        className="bg-light-grey-color text-dark-color"
+                        onClick={closeModal}
+                      >
+                        닫기
+                      </Button>
+                    </>
+                  ),
+                })
               }}
             />
           </div>
@@ -411,9 +379,7 @@ const AllGuestBookModal = ({
         {pages[0] > 0 && (
           <div
             className="page"
-            onClick={() => {
-              loadPage(pages[0] - 1)
-            }}
+            onClick={() => loadPage(pages[0] - 1)}
           >
             이전
           </div>
@@ -422,10 +388,7 @@ const AllGuestBookModal = ({
           <div
             className={`page${page === currentPage ? " current" : ""}`}
             key={page}
-            onClick={() => {
-              if (page === currentPage) return
-              loadPage(page)
-            }}
+            onClick={() => page !== currentPage && loadPage(page)}
           >
             {page + 1}
           </div>
@@ -433,9 +396,7 @@ const AllGuestBookModal = ({
         {pages[pages.length - 1] < totalPages - 1 && (
           <div
             className="page"
-            onClick={() => {
-              loadPage(pages[pages.length - 1] + 1)
-            }}
+            onClick={() => loadPage(pages[pages.length - 1] + 1)}
           >
             다음
           </div>
@@ -471,9 +432,7 @@ const DeleteGuestBookModal = ({
           }
 
           if (password.length > RULES.password.maxLength) {
-            alert(
-              `비밀번호를 ${RULES.password.maxLength}자 이하로 입력해주세요.`,
-            )
+            alert(`비밀번호를 ${RULES.password.maxLength}자 이하로 입력해주세요.`)
             return
           }
 
