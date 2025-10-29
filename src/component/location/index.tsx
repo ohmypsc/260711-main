d379cb84846fdce65d5310e28dd0f463cafd66a9
ohmypsc <greatkto@gmail.com>
2025-10-29 09:41:16 +0900

    Update index.tsx

--- src/component/location/index.tsx
+++ src/component/location/index.tsx
@@ -15,7 +15,9 @@ export const Location = () => {
         </div>
         <Map />
       </LazyDiv>
+
       <LazyDiv className="card location">
+        {/* 🚍 대중교통 안내 */}
         <div className="location-info">
           <div className="transportation-icon-wrapper">
             <BusIcon className="transportation-icon" />
@@ -23,32 +25,31 @@ export const Location = () => {
           <div className="heading">대중교통</div>
           <div />
           <div className="content">
-            * 지하철 이용시
+            * 지하철 이용 시(돌다리 얘기 넣어 말아 고민고민쓰)
             <br />
-            지하철 2호선 <b>낙성대역 4번출구</b> 나와서
+            <b>대전 1호선 유성온천역</b> 하차 후 도보 약 8분 거리
             <br />
-            → 첫번째 골목 끼고 좌회전
-            <br />→ 마을버스 <b>관악 02번</b> 승차
-            <br />→ <b>서울대후문·연구공원 정류장</b> 하차
+            또는 <b>시청방면 104, 706번 버스</b> 이용
             <br />
-            → 길 건너 간판 참고해 도보로 100m 이동
+            → <b>유성컨벤션웨딩홀 정류장</b> 하차
             <br />
-            검은색 피라미드 유리 건물입니다.
+            <b>웨딩홀 3층 그랜드홀</b>로 오시면 됩니다.
           </div>
           <div />
           <div className="content">
             * 버스 이용 시
             <br />
-            - 간선(파랑): 461, 641
+            - 간선버스: 104, 706, 911
             <br />
-            - 지선(초록): 5413, 5524, 5528
+            - 지선버스: 119, 121, 655
             <br />
-            반드시 <b>낙성대입구</b> 하차
-            <br />→ 마을버스 <b>관악 02번</b> 이용
+            <b>유성온천역 또는 유성컨벤션웨딩홀 정류장</b> 하차 후 도보 이동
             <br />
-            이하 위와 동일합니다.
+            유성문화원 방향으로 약 100m 직진하시면 웨딩홀이 보입니다.
           </div>
         </div>
+
+        {/* 🚗 자가용 안내 */}
         <div className="location-info">
           <div className="transportation-icon-wrapper">
             <CarIcon className="transportation-icon" />
@@ -56,19 +57,19 @@ export const Location = () => {
           <div className="heading">자가용</div>
           <div />
           <div className="content">
-            네이버 지도, 카카오 네비, 티맵 등 이용
+            내비게이션에서 <b>유성컨벤션웨딩홀</b> 검색
             <br />
-            <b>서울대학교 연구공원 웨딩홀</b> 검색
+            (주소: 대전광역시 유성구 온천북로 77)
             <br />
-            - 주차 요금은 무료입니다.
+            <b>유성온천역</b> 사거리에서 약 3분 거리입니다.
             <br />
-            (주차장 이용 시 웨딩홀과 바로 연결)
+            웨딩홀 내 전용 주차장이 있으며 <b>무료 주차</b> 가능합니다.
           </div>
           <div />
           <div className="content">
             <b>
-              ※ 서울대학교 정, 후문을 통과할 경우 통행료가 발생하므로
-              유의바랍니다. 낙성대 방향으로 이용해주세요.
+              ※ 주차장 진입 시 ‘그랜드홀’ 방향으로 진입하시면
+              예식장과 바로 연결됩니다.
             </b>
           </div>
         </div>
