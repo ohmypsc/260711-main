bfac47cfffb4a685c60bd09b766521238d73ad92
ohmypsc <greatkto@gmail.com>
2025-10-30 11:38:52 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,20 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./App"
-import { ModalProvider } from "./component/modal"
+import Router from "./App" // App 이름이 Router로 export 되어 있음
+import { ModalProvider } from "./component/modal/provider" // ✅ 정확한 Provider import 경로
 import { StoreProvider } from "./component/store"
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+)
+
 root.render(
   <React.StrictMode>
+    {/* ✅ 모달과 전역 스토어를 앱 전체에 적용 */}
     <ModalProvider>
       <StoreProvider>
-        <App />
+        <Router /> {/* App이 Router로 export된 상태 */}
       </StoreProvider>
     </ModalProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
