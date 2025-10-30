d55ec49bd3c3f641930836ae712ed3ed92d222ad
ohmypsc <greatkto@gmail.com>
2025-10-30 11:05:04 +0900

    Update index.tsx

--- src/component/cover/index.tsx
+++ src/component/cover/index.tsx
@@ -1,51 +1,27 @@
-import {
-  BRIDE_FULLNAME,
-  GROOM_FULLNAME,
-  LOCATION,
-  WEDDING_DATE,
-  WEDDING_DATE_FORMAT,
-} from "../../const"
+.cover {
+  text-align: center;
+  font-family: "MapoGoldenPier", "Noto Sans KR", sans-serif;
 
-const DAY_OF_WEEK = [
-  "일요일",
-  "월요일",
-  "화요일",
-  "수요일",
-  "목요일",
-  "금요일",
-  "토요일",
-]
+  .names {
+    font-size: 1.5rem;
+    margin: 1.2rem 0;
+    .divider {
+      color: var(--theme-color);
+      margin: 0 0.3rem;
+    }
+  }
 
-export const Cover = () => {
-  return (
-    <div className="card cover">
-      {/* 날짜 */}
-      <div className="wedding-date">
-        {WEDDING_DATE.format("YYYY")}
-        <div className="divider" />
-        {WEDDING_DATE.format("MM")}
-        <div className="divider" />
-        {WEDDING_DATE.format("DD")}
-      </div>
+  .family {
+    margin-top: 2rem;
+    font-size: 0.95rem;
+    line-height: 1.5;
+  }
 
-      {/* 요일 */}
-      <div className="wedding-day-of-week">
-        {DAY_OF_WEEK[WEDDING_DATE.day()]}
-      </div>
-
-      {/* 안내 문구 */}
-      <div className="subtitle">소중한 날에 함께해 주세요</div>
-
-      {/* 이름 */}
-      <div className="names">
-        {GROOM_FULLNAME}
-        <div className="divider" />
-        {BRIDE_FULLNAME}
-      </div>
-
-      {/* 날짜 + 장소 */}
-      <div className="info">{WEDDING_DATE.format(WEDDING_DATE_FORMAT)}</div>
-      <div className="info">{LOCATION}</div>
-    </div>
-  )
+  button {
+    margin-top: 1.8rem;
+    background: var(--theme-color);
+    color: white;
+    padding: 0.6rem 1.2rem;
+    border-radius: 8px;
+  }
 }
