6cf9bad57691d0436f8a3fa529090fad1de96fff
ohmypsc <greatkto@gmail.com>
2025-10-30 11:48:34 +0900

    Update index.tsx

--- src/component/button/index.tsx
+++ src/component/button/index.tsx
@@ -1,17 +1,23 @@
-import { ButtonHTMLAttributes } from "react"
+import { ButtonHTMLAttributes, ReactNode } from "react"
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   buttonStyle?: "style1" | "style2"
+  children?: ReactNode // ✅ children 추가
 }
 
 export const Button = (props: ButtonProps) => {
-  const { buttonStyle, ...rest } = props
+  const { buttonStyle, children, ...rest } = props
+
   return (
     <button
       {...{
         ...rest,
-        className: `button ${buttonStyle === "style2" ? "button-style-2" : "button-style-1"}${props.className ? " " + props.className : ""}`,
+        className: `button ${
+          buttonStyle === "style2" ? "button-style-2" : "button-style-1"
+        }${props.className ? " " + props.className : ""}`,
       }}
-    />
+    >
+      {children} {/* ✅ 버튼 내용 표시 */}
+    </button>
   )
 }
