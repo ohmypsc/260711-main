4dcac32a6165b8c5a99513c36b3d0037d4403243
ohmypsc <greatkto@gmail.com>
2025-10-30 10:47:21 +0900

    Update index.tsx

--- src/component/shareButton/index.tsx
+++ src/component/shareButton/index.tsx
@@ -11,49 +11,40 @@ import ktalkIcon from "../../icons/ktalk-icon.png"
 import { LazyDiv } from "../lazyDiv"
 import { useKakao } from "../store"
 
-const baseUrl = import.meta.env.BASE_URL.replace(/\/$/, "") // 끝 슬래시 제거
-
 export const ShareButton = () => {
   const kakao = useKakao()
 
+  const handleShare = () => {
+    if (!kakao) return
+
+    kakao.Share.sendDefault({
+      objectType: "location",
+      address: SHARE_ADDRESS,
+      addressTitle: SHARE_ADDRESS_TITLE,
+      content: {
+        title: `${GROOM_FULLNAME} ❤️ ${BRIDE_FULLNAME}의 결혼식에 초대합니다.`,
+        description: WEDDING_DATE.format(WEDDING_DATE_FORMAT) + "\n" + LOCATION,
+        imageUrl: "https://ohmypsc.github.io/260711-main/preview_image.png",
+        link: {
+          mobileWebUrl: "https://ohmypsc.github.io/260711-main",
+          webUrl: "https://ohmypsc.github.io/260711-main",
+        },
+      },
+      buttons: [
+        {
+          title: "초대장 보기",
+          link: {
+            mobileWebUrl: "https://ohmypsc.github.io/260711-main",
+            webUrl: "https://ohmypsc.github.io/260711-main",
+          },
+        },
+      ],
+    })
+  }
+
   return (
     <LazyDiv className="footer share-button">
-      <button
-        className="ktalk-share"
-        onClick={() => {
-          if (!kakao) return
-
-          kakao.Share.sendDefault({
-            objectType: "location",
-            address: SHARE_ADDRESS,
-            addressTitle: SHARE_ADDRESS_TITLE,
-            content: {
-              title: `${GROOM_FULLNAME} ❤️ ${BRIDE_FULLNAME}의 결혼식에 초대합니다.`,
-              description:
-                WEDDING_DATE.format(WEDDING_DATE_FORMAT) + "\n" + LOCATION,
-              imageUrl:
-                `${window.location.protocol}//${window.location.host}${baseUrl}/preview_image.png`,
-              link: {
-                mobileWebUrl:
-                  `${window.location.protocol}//${window.location.host}${baseUrl}`,
-                webUrl:
-                  `${window.location.protocol}//${window.location.host}${baseUrl}`,
-              },
-            },
-            buttons: [
-              {
-                title: "초대장 보기",
-                link: {
-                  mobileWebUrl:
-                    `${window.location.protocol}//${window.location.host}${baseUrl}`,
-                  webUrl:
-                    `${window.location.protocol}//${window.location.host}${baseUrl}`,
-                },
-              },
-            ],
-          })
-        }}
-      >
+      <button className="ktalk-share" onClick={handleShare}>
         <img src={ktalkIcon} alt="ktalk-icon" /> 카카오톡으로 공유하기
       </button>
     </LazyDiv>
