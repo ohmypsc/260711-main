b220c1eb6f3788f9174c952b9aead28361047e95
ohmypsc <greatkto@gmail.com>
2025-11-03 10:41:00 +0900

    Update index.tsx

--- src/component/invitation/index.tsx
+++ src/component/invitation/index.tsx
@@ -20,29 +20,17 @@ export const Invitation = () => {
         어린 시절 순결한 비누 거품 속에서 우리가 했던 맹세들을 찾아
         너의 팔에 모두 적어줄게
       </div>
-      <div className="content">
-        내가 나를 찾는 술래였던 시간을 모두 돌려줄게
-      </div>
-      <div className="content">
-        나는 오래된 거리처럼 너를 사랑하고 벌들은 귓속의 별들처럼 웅성거리고
-      </div>
-      <div className="content">
-        나는 인류가 아닌 단 한 여자를 위해 쓴잔을 죄다 마시겠지
-      </div>
-      <div className="content">
-        슬픔이 나의 물컵에 담겨 있다 투명 유리 조각처럼
-      </div>
 
-      <div className="content poet">- 진은영, &lt;청혼&gt;</div>
+      <div className="content poet">- 진은영, &lt;청혼&gt; 중</div>
 
       <div className="break" />
 
       <div className="content">
         오래된 거리처럼 익숙하지만,<br />
-        시간의 흐름에 기대지 않고 서로에게 최선을 다하며,<br />
-        어린 시절의 순수한 마음으로, 여름 비도 즐거워하고,<br />
-        유리처럼 날카로운 슬픔도 함께하는 부부가 되고자 합니다.<br />
-        이 시작을 함께해 주신다면 감사하겠습니다.
+        여름 비도 은색 드럼처럼 즐거워하고,<br />
+        시간에게 기대지 않고 서로에게 최선을 다하며,<br />
+        어린 시절 순수한 마음으로 서로를 대하는 부부가 되고자 합니다.<br />
+        이 시작을 함께해 주신다면 더없이 감사하겠습니다.
       </div>
     </LazyDiv>
   )
