ae80e40bf14bc89afe8be63ec06ea622f951149a
ohmypsc <greatkto@gmail.com>
2025-11-05 14:35:38 +0900

    Update index.tsx

--- src/component/cover/index.tsx
+++ src/component/cover/index.tsx
@@ -30,7 +30,7 @@ export const Cover = () => {
       {/* 이름 */}
       <div className="names">
         {GROOM_FULLNAME}
-       <div className="divider">✦</div>
+        <div className="divider">✦</div>
         {BRIDE_FULLNAME}
       </div>
 
@@ -84,7 +84,8 @@ export const Cover = () => {
               </div>
             ),
             content: (
-              <>
+              /* ✅ 오직 이 부분만 수정됨 */
+              <div className="content">
                 <div className="contact-info">
                   {GROOM_INFO.filter(({ phone }) => !!phone).map(
                     ({ relation, name, phone }) => (
@@ -134,7 +135,7 @@ export const Cover = () => {
                     ),
                   )}
                 </div>
-              </>
+              </div>
             ),
             footer: (
               <Button
