56fd59af5daccad367feca0400a4c04e47267b87
ohmypsc <greatkto@gmail.com>
2025-11-06 11:12:31 +0900

    Update index.tsx

--- src/component/location/index.tsx
+++ src/component/location/index.tsx
@@ -9,68 +9,44 @@ export const Location = () => {
     <>
       <LazyDiv className="card location">
         <h2>오시는 길</h2>
+
         <div className="addr">
-          {LOCATION}
+          <strong>{LOCATION}</strong>
           <div className="detail">{LOCATION_ADDRESS}</div>
         </div>
+
         <Map />
       </LazyDiv>
 
       <LazyDiv className="card location">
-        {/* 🚍 대중교통 안내 */}
+        {/* 🚍 대중교통 */}
         <div className="location-info">
-          <div className="transportation-icon-wrapper">
-            <BusIcon className="transportation-icon" />
-          </div>
-          <div className="heading">대중교통</div>
-          <div />
+          <BusIcon className="transportation-icon" />
+          <div className="heading">대중교통(돌다리얘기넣어말아고민고민</div>
+
           <div className="content">
-            * 지하철 이용 시<br />(돌다리 얘기 넣어 말아 고민고민쓰)
-            <br />
-            <b>대전 1호선 유성온천역</b> 하차 후 도보 약 8분 거리
-            <br />
-            또는 <b>시청방면 104, 706번 버스</b> 이용
-            <br />
-            → <b>유성컨벤션웨딩홀 정류장</b> 하차
-            <br />
-            <b>웨딩홀 3층 그랜드홀</b>로 오시면 됩니다.
-          </div>
-          <div />
-          <div className="content">
-            * 버스 이용 시
-            <br />
-            - 간선버스: 104, 706, 911
-            <br />
-            - 지선버스: 119, 121, 655
-            <br />
-            <b>유성온천역 또는 유성컨벤션웨딩홀 정류장</b> 하차 후 도보 이동
-            <br />
-            유성문화원 방향으로 약 100m 직진하시면 웨딩홀이 보입니다.
+            <p><b>유성온천역 (1호선)</b> 하차 후 도보 약 8분</p>
+            <p>
+              또는 <b>104번 / 706번</b> 버스 이용 →{" "}
+              <b>유성컨벤션웨딩홀 정류장</b> 하차
+            </p>
+            <p><b>웨딩홀 3층 그랜드홀</b>에서 예식이 진행됩니다.</p>
           </div>
         </div>
 
-        {/* 🚗 자가용 안내 */}
+        {/* 🚗 자가용 */}
         <div className="location-info">
-          <div className="transportation-icon-wrapper">
-            <CarIcon className="transportation-icon" />
-          </div>
+          <CarIcon className="transportation-icon" />
           <div className="heading">자가용</div>
-          <div />
+
           <div className="content">
-            내비게이션에서 <b>유성컨벤션웨딩홀</b> 검색
-            <br />
-            (주소: 대전광역시 유성구 온천북로 77)
-            <br />
-            <b>유성온천역</b> 사거리에서 약 3분 거리입니다.
-            <br />
-            웨딩홀 내 전용 주차장이 있으며 <b>무료 주차</b> 가능합니다.
-          </div>
-          <div />
-          <div className="content">
-            <b>
-              ※ 주차장 진입 시 ‘그랜드홀’ 방향으로 진입하시면
-              예식장과 바로 연결됩니다.
-            </b>
+            <p>
+              내비게이션에서 <b>유성컨벤션웨딩홀</b> 검색  
+              <br />(대전 유성구 온천북로 77)
+            </p>
+            <p className="note">
+              주차 요금은 무료입니다.
+            </p>
           </div>
         </div>
       </LazyDiv>
